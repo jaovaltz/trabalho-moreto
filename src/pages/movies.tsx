d944b89936ffcd57b2b63671f0ad468862a78c0e
wellapos <wellington258@live.com>
2023-06-15 19:42:32 -0300

    feat: setup tailwind plugins

---
+++ src/pages/movies.tsx
@@ -0,0 +1,7 @@
+export default function Movies() {
+  return (
+    <div className="flex flex-col divide-white text-clip bg-blue-50 from-fuchsia-700 drop-shadow-md">
+      movies
+    </div>
+  )
+}
